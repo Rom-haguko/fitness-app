c49cef01ff3bbe01b61467f8f32d60054f61c428
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-04-13 13:13:33 +0800

    Made entity User for sql-table users

---
+++ java-service/src/main/java/com/fitness/fitnessapp/entity/User.java
@@ -0,0 +1,31 @@
+package com.fitness.fitnessapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users", schema="fitness_tracker")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+    
+    private String role = "ROLE_USER";
+}
