d777eb45e27761d0b27b704b24797104783bd242
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-04-25 21:35:35 +0800

    Added annotation @Repository

--- java-service/src/main/java/com/fitness/fitnessapp/repository/UserRepository.java
+++ java-service/src/main/java/com/fitness/fitnessapp/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.fitness.fitnessapp.repository;
 
 import com.fitness.fitnessapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
