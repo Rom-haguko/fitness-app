8d35683ee316124c25ccb5572d1252e18a6474e4
Alexander <sashashem2505006@gmail.com>
2026-04-29 15:52:06 +0800

    added googleai api settings in executable file

--- python-service/main.py
+++ python-service/main.py
@@ -1,4 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+import google.generativeai as genai
+
+
+# api settings
+class Settings(BaseSettings):
+    google_api_key: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+genai.configure(api_key=settings.google_api_key)
+
+
+
 
 app = FastAPI()
 
