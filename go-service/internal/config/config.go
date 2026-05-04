9dc65adc15f2367114bb51b3d18c91f633569f47
RomG <romagureev170@gmail.com>
2026-05-04 12:52:26 +0800

    Add config loader

---
+++ go-service/internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	AppEnv string
+	AppPort string
+	DBHost string
+	DBPort string
+	DBName string
+	DBUser string
+	DBPassword string
+}
+
+func (c Config) Addr() string {
+	return ":" + c.AppPort
+}
+
+func (c Config) DSN() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		c.DBUser,
+		c.DBPassword,
+		c.DBHost,
+		c.DBPort,
+		c.DBName,
+	)
+}
+
+func Load() Config {
+	return Config{
+		AppEnv:     getEnv("APP_ENV", "dev"),
+		AppPort:    getEnv("APP_PORT", "8081"),
+		DBHost:     getEnv("DB_HOST", "db"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBName:     getEnv("DB_NAME", "fitness_tracker"),
+		DBUser:     getEnv("DB_USER", "user"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
