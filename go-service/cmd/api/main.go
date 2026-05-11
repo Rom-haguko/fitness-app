9a70b81780105d4048465bb38ef578e2c6ac0140
RomG <romagureev170@gmail.com>
2026-05-11 23:40:22 +0800

    Add HTTP middleware

--- go-service/cmd/api/main.go
+++ go-service/cmd/api/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Go service started!")
+	fmt.Println("Go service started!")
 }
--- go-service/internal/httpserver/router.go
+++ go-service/internal/httpserver/router.go
@@ -1,16 +1,20 @@
 package httpserver
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/Rom-haguko/fitness-app/go-service/internal/handler"
 )
 
-func NewRouter() http.Handler {
+func NewRouter(log *slog.Logger) http.Handler {
 	mux := http.NewServeMux()
 
 	healthHandler := handler.NewHealthHandler()
 
 	mux.HandleFunc("/health", healthHandler.Health)
-	return mux
+	
+	var root http.Handler = mux
+	root = LoggingMiddleware(log, RecoverMiddleware(log, root))
+	return root
 }
