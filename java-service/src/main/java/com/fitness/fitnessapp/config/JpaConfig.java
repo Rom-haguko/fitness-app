45a3efbecfe3cde72227e63a49b03e564dafd357
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-05-12 14:20:29 +0800

    configured jpa, mvc and webclient configs

---
+++ java-service/src/main/java/com/fitness/fitnessapp/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.fitness.fitnessapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.fitness.fitnessapp.repository")
+public class JpaConfig {
+}
