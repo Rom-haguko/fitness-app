61dd3bdff9789ba1815ce10a2d1ffa438b994cbf
Alexander <sashashem2505006@gmail.com>
2026-05-17 20:12:26 +0800

    make service stateless and improve AI prompt

--- python-service/app/api/workout_router.py
+++ python-service/app/api/workout_router.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from app.db.database import get_db
+from fastapi import APIRouter
 from app.schemas.workout import WorkoutRequest, WorkoutResponse
 from app.services.workout_service import create_workout_plan
 from app.services.ai_service import AIService
@@ -12,6 +10,7 @@ router = APIRouter()
 ai_service = AIService(api_key=os.getenv("GOOGLE_API_KEY"))
 
 @router.post("/generate", response_model=WorkoutResponse)
-async def generate_workout(request: WorkoutRequest, db: Session = Depends(get_db)):
-    plan = await create_workout_plan(db, ai_service, request)
+async def generate_workout(request: WorkoutRequest):
+
+    plan = await create_workout_plan(ai_service, request)
     return plan
--- python-service/app/services/workout_service.py
+++ python-service/app/services/workout_service.py
@@ -1,26 +1,14 @@
-from sqlalchemy.orm import Session
-from app.models.workout_plan import WorkoutPlan
+from app.services.ai_service import AIService
 from app.schemas.workout import WorkoutRequest
 
-async def create_workout_plan(db: Session, ai_service, request: WorkoutRequest):
+async def create_workout_plan(ai_service: AIService, request: WorkoutRequest):
 
-    ai_generated_content = await ai_service.generate_plan_json(request)
+    ai_content = await ai_service.generate_plan_json(request)
     
-
-    db_plan = WorkoutPlan(
-        user_id=request.user_id,
-        goal=request.goal,
-        level=request.level,
-        days_per_week=request.days_per_week
-    )
-    db.add(db_plan)
-    db.commit()
-
-
     return {
         "goal": request.goal,
         "level": request.level,
         "days_per_week": request.days_per_week,
         "split_type": request.split_type,
-        "workout_plan": ai_generated_content["workout_plan"]
+        "workout_plan": ai_content["workout_plan"]
     }
